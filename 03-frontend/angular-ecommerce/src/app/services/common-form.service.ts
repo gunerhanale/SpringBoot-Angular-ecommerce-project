eefe17a87ee8eea0581055cba749c663e038c08a
gunerhanale <gunerhanale@gmail.com>
2021-01-17 23:39:11 +0100

    coded service methods to retrieve Country and States consuming Rest API

--- 03-frontend/angular-ecommerce/src/app/services/common-form.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/common-form.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Interface } from 'readline';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Country } from '../common/country';
+import { State } from '../common/state';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +15,23 @@ export class CommonFormService {
   private statesUrl = "http://localhost:8080/api/states";
 
   constructor(private httpClient: HttpClient) { }
+
+  getCountries(): Observable<Country[]> {
+
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+
+    // search url
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
+  }
 
   getCreditCardMonths(startMonth: number): Observable<number[]>{
 
@@ -43,3 +64,17 @@ export class CommonFormService {
     return of(data);
   }
 }
+
+interface GetResponseCountries {
+  _embedded:{
+    countries: Country[];
+  }
+}
+
+interface GetResponseStates {
+  _embedded:{
+    states: State[];
+  }
+}
+
+
