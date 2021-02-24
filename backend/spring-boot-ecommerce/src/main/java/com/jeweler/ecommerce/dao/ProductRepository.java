a6f09247a50897c0184ebfd0457b1ff23a3ebb90
gunerhanale <gunerhanale@gmail.com>
2021-02-24 23:23:06 +0100

    added native query method searching with two different parameters

--- backend/spring-boot-ecommerce/src/main/java/com/jeweler/ecommerce/dao/ProductRepository.java
+++ backend/spring-boot-ecommerce/src/main/java/com/jeweler/ecommerce/dao/ProductRepository.java
@@ -4,6 +4,7 @@ import com.jeweler.ecommerce.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,5 +14,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
     Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
+    @Query(value = "SELECT * FROM product p WHERE p.name like %?1% OR p.description like %?2%", nativeQuery = true)
+    Page<Product> findByNameOrDescription(@RequestParam("name") String name, @RequestParam("desc") String desc, Pageable pageable);
 
 }
